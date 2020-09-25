480eaf638faf539195c9aa1d42ece735695dfa4c
ericgaspar <junk.eg@free.fr>
2020-09-25 13:19:00 +0200

    fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="postgresql apt-transport-https"
+#pkg_dependencies="postgresql apt-transport-https"
 
 nodejs_version=12
 
@@ -20,3 +20,16 @@ nodejs_version=12
 #=================================================
 # FUTURE OFFICIAL HELPERS
 #=================================================
+
+# Execute a command as another user
+# usage: ynh_exec_as USER COMMAND [ARG ...]
+ynh_exec_as() {
+  local USER=$1
+  shift 1
+
+  if [[ $USER = $(whoami) ]]; then
+    eval "$@"
+  else
+    sudo -u "$USER" "$@"
+  fi
+}
